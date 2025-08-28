21e05aad643531172df7bcb8511080beb0be48be
Farid Ahmed <farid.ahmed@hostupcloud.com>
2025-08-28 09:41:22 +0530

    nested interface

---
+++ java_practise/NestedInterface.java
@@ -0,0 +1,23 @@
+interface Outer{
+    void show();
+    interface Inner{
+        void display();
+    }
+}
+
+class Test implements Outer.Inner{
+    public void display(){
+        System.out.println("display method");
+    }
+    public void show(){
+        System.out.println("show method");
+    }
+}
+
+public class NestedInterface{
+    public static void main(String[] args){
+        Test t=new Test();
+        t.display();
+        t.show();
+    }
+}
